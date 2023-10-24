95d02c67f014e02d94df563c0e8cad22c650609c
Zeyuan Wong <ipc01048@student.bxcl.edu.my>
2023-10-24 11:20:47 +0800

    add item to Queue

--- Queue.py
+++ Queue.py
@@ -1,3 +1,4 @@
+#Create a new Queue
 EMPTY_STRING = ""
 NULL_POINTER = -1
 MAX_QUEUE_SIZE = 8
@@ -13,3 +14,15 @@ def InitialiseQueue():
     FrontOfQueuePointer = NULL_POINTER
     EndOfQueuePointer = NULL_POINTER
     NumberInQueue = 0
+
+#Add Item to the Queue
+def Enqueue(NewItem):
+    if NumberInQueue < MAX_QUEUE_SIZE
+        EndOfQueuePointer = EndOfQueuePointer + 1
+
+        if EndOfQueuePointer > MAX_QUEUE_SIZE - 1:
+            EndOfQueuePointer = 0
+        
+        Queue[EndOfQueuePointer] = NewItem
+
+        NumberInQueue = NumberInQueue + 1

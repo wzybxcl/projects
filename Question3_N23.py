4ec7aece99fd3c952ca177c15ba62191f0c10597
wzybxcl <148737550+wzybxcl@users.noreply.github.com>
2024-02-02 03:21:02 +0000

    update

--- Question3_N23.py
+++ Question3_N23.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 class Character:
     def __init__(self, charactername, dateofbirth, intelligence, speed):
         self.CharacterName = charactername
@@ -20,9 +18,16 @@ class Character:
         self.Intelligence = self.Intelligence*1.1
     
     def ReturnAge(self):
-        birthyear = self.DateOfBirth.year
+        birthyear = int(self.DateOfBirth.split()[-1])
         currentyear = 2023
         age = currentyear - birthyear
 
         return age
+
+FirstCharacter = Character("Royal", "1 January 2019", 70, 30)
+
+FirstCharacter.Learn()
+print(f"The name of the character is {FirstCharacter.GetName()}, their age is {FirstCharacter.ReturnAge()}, their intelligence is {FirstCharacter.GetIntelligence()}")
+
+
 

eeea4fd03574d44777a27d24d688ebeb4d84ae06
wzybxcl <148737550+wzybxcl@users.noreply.github.com>
2024-02-05 03:56:02 +0000

    update

--- Question3_N23.py
+++ Question3_N23.py
@@ -1,9 +1,9 @@
 class Character:
     def __init__(self, charactername, dateofbirth, intelligence, speed):
-        self.CharacterName = charactername
-        self.DateOfBirth = dateofbirth
-        self.Intelligence =  intelligence
-        self.Speed = speed
+        self.CharacterName = charactername #string
+        self.DateOfBirth = dateofbirth #string
+        self.Intelligence =  intelligence #real number
+        self.Speed = speed #integer
     
     def GetIntelligence(self):
         return self.Intelligence
@@ -29,5 +29,22 @@ FirstCharacter = Character("Royal", "1 January 2019", 70, 30)
 FirstCharacter.Learn()
 print(f"The name of the character is {FirstCharacter.GetName()}, their age is {FirstCharacter.ReturnAge()}, their intelligence is {FirstCharacter.GetIntelligence()}")
 
+class MagicCharacter(Character):
+    def __init__(self, element, charactername, dateofbirth, intelligence, speed):
+        super().__init__(charactername, dateofbirth, intelligence, speed)
+        self.Element = element
 
+    def Learn(self):
+        if self.Element in ["water", "fire"]:
+            intelligenceincrease = 1.2
+        elif self.Element == "earth":
+            intelligenceincrease = 1.3
+        else:
+            intelligenceincrease = 1.1
+        
+        self.Intelligence = self.Intelligence * intelligenceincrease
 
+FirstMagic = MagicCharacter("fire", "Light", "28 January 2018", 70, 22)
+
+FirstMagic.Learn()
+print(f"The name of the character is {FirstMagic.GetName()}, their age is {FirstMagic.ReturnAge()}, their intelligence is {FirstMagic.GetIntelligence()}")
---
+++ question3TreasureChest.py
@@ -0,0 +1,15 @@
+class TreasureChest:
+    def __init__(self, question, answer, points):
+        self.__question = question #string
+        self.__answer = answer #integer
+        self.__points = points #integer
+
+    def readData():
+        try:
+            file = open("TreasureChestData.txt", "r")
+            for i in file:
+                
+
+
+        except: 
+            IOError

052252e3534a00f7c9ee6888435e840d0aad9b5c
wzybxcl <148737550+wzybxcl@users.noreply.github.com>
2024-02-21 06:38:59 +0000

    final version of binary tree task

--- Feb21Paper4/ArrayNodes.py
+++ Feb21Paper4/ArrayNodes.py
@@ -17,5 +17,19 @@ def search_value(root, value_to_find, array_nodes):
     if array_nodes[root][1] > value_to_find:
         return search_value(array_nodes[root][2], value_to_find, array_nodes)
     
-    
+def post_order(root):
+    if root is not None:
+
+        post_order(root.left)
+        post_order(root.right)
+        print(root.value)
+
+result = search_value(0, 15, ArrayNodes)
+if result != -1:
+    print(f"Value 15 found at index {result}")
+else:
+    print("Value 15 not found in the tree")
+
+post_order(0, ArrayNodes)
+
 
